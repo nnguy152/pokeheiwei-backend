b5731f0d419fbafee0ee29fcf2a6507658ba476f
Nhi Nguyen <nnguy152@gmail.com>
2018-04-12 19:52:25 -0400

    added one comment to pokeseed file lol

--- db/pokeseed.js
+++ db/pokeseed.js
@@ -1,6 +1,7 @@
 const Pokemon = require('../models/Pokemon')
 const fetch = require('node-fetch')
 
+// have to go through api maybe 20 at a time so you don't getthe api mad at you
 
 Pokemon.find({}).then(() => {
    for (var i = 780; i < 800; i++) {
